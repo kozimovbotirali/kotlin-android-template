f1bec4e620afcc0524a30704fcc5bd9d29009cef
Botirali Kozimov <55086570+kozimovbotirali@users.noreply.github.com>
2020-06-25 00:39:17 +0500

    Update Coordinates.kt

--- buildSrc/src/main/java/Coordinates.kt
+++ buildSrc/src/main/java/Coordinates.kt
@@ -1,7 +1,7 @@
-val PUBLISHING_GROUP = "com.ncorti.kotlin.template"
+val PUBLISHING_GROUP = "uz.xdevelop"
 
 object AppCoordinates {
-    const val APP_ID = "com.ncorti.kotlin.template.app"
+    const val APP_ID = "uz.xdevelop.app"
 
     const val APP_VERSION_NAME = "1.0.0"
     const val APP_VERSION_CODE = 1
